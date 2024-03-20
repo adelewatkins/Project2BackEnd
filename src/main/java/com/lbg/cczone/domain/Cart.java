04cdf060098aeaccf20cae1e5c69ffd9df889a16
treesacherian <treesa.cherian@gmail.com>
2024-03-20 08:40:52 +0000

    buyer userid and password added

--- src/main/java/com/lbg/cczone/domain/Cart.java
+++ src/main/java/com/lbg/cczone/domain/Cart.java
@@ -18,8 +18,10 @@ public class Cart {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String buyer;
-	// @JsonProperty("cartItemQuantity")
-//	private Integer cartItemQuantity;
+	private String userId;
+	private String password;
+	private String tel;
+	private String address;
 
 	@JsonManagedReference
 	@OneToMany(mappedBy = ("cart"))
@@ -30,11 +32,48 @@ public class Cart {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Cart(Integer id, String buyer, List<Item> items) {
+	public Cart(Integer id, String buyer, String userId, String password, String tel, String address,
+			List<Item> items) {
 //		super();
 		this.id = id;
 		this.buyer = buyer;
+		this.userId = userId;
+		this.password = password;
+		this.tel = tel;
+		this.address = address;
 		this.items = items;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
 	}
 
 	public Integer getId() {
